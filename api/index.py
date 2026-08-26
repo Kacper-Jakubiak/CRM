0218b7e67b4dee995ee821d6718de973b5194beb
Kacper Jakubiak <kajakubiak@student.agh.edu.pl>
2026-08-26 19:01:21 +0200

    Revert "warmup"
    This reverts commit 5b75f32d9f9f2710a73e90980b4fb9caa9b61c4a.

--- api/index.py
+++ api/index.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
 
 from api.auth import router as auth_router, verify_token
 from routers import courses, emails, customers, integrations
-from db import engine
-from logger import logger
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    try:
-        with engine.connect():
-            logger.info("Database pool connection established successfully.")
-        
-    except Exception as e:
-        logger.warning(f"Warning: Database warm-up failed: {e}")
-        
-    yield
-    
-    engine.dispose()
-    logger.info("Database engine disposed.")
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
